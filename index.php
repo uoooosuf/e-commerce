5fbd18aa76d0e446f5f6eaebc8c1d28dafd1cd72
Yusuf Abubakar <uoooosuf@gmail.com>
2019-06-15 06:06:24 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,284 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" type="image/ico" href="img/favicon.ico">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+    <title>Home || Mackendi LTD</title>
+    <style>
+        body{
+            padding-top: 54px;
+        }
+        
+        @media(min-width:992px){
+            body{
+                padding-top: 56px;
+            }
+        }
+      
+    </style>
+  </head>
+  <body class="bg-secondary">
+    
+    
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container">
+            <a href="#" class="navbar-brand">Mack-ltd</a>
+            <button type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle Navigation" class="navbar-toggler">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+               <ul class="navbar-nav ml-auto">
+                   <li class="nav-item">
+                   <a class="nav-link active" href="#">Services</a></li>
+                   <span class="sr-only">(Current)</span>
+                   <li class="nav-item">
+                   <a class="nav-link" href="#">about</a></li>
+                   <li class="nav-item">
+                   <a class="nav-link" href="#">Marketing</a></li>
+               </ul>
+            </div><!-- end collapse div -->
+        </div><!-- end container div -->
+    </nav>
+
+  <p class="mt-5">&nbsp;</p>
+ 
+ <!-- Two Columns -->
+ <div class="container">
+ <div class="row">
+     <div class="col-lg-3">
+         <h1 class="my-4 text-center text-dark">Shop Now!</h1>
+         <div class="list-group">
+             <a href="#" class="list-group-item bg-dark text-muted">Laptops</a>
+             <a href="#" class="list-group-item bg-dark text-muted">Mobile Phones</a>
+             <a href="#" class="list-group-item bg-dark text-muted">other Accessories</a>
+         </div>
+  <p class="mb-5">&nbsp;</p>   
+     </div><!-- end left column -->
+     <div class="col-lg-9">
+     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="img/ballpen-blur-close-up-461077.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img/app-blur-cell-phone-887751.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img/ballpen-blur-close-up-461077.jpg" alt="Third slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img/app-blur-cell-phone-887751.jpg" alt="Fourth slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+     <div class="row mt-4">
+     <!-- CLASSES -->
+     
+        <!--Class 1 -->
+        <!-- -->
+        <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+             <a href="#"><img src="img/app-blur-cell-phone-887751.jpg" alt="#" title="#" class="card-img-top"></a>
+             <div class="card-body">
+                 <h5 class="card-title"><a href="#">Iphone X</a></h5>
+                 <h6 class="text-muted">Description</h6>
+                 <p class="card-text">500Gb RAM , <br> 30pixel camera</p>
+                 <p>&#9733;&#9733;&#9733;&#9733;&#9733;&#9734;</p>
+             </div><!--end card body-->
+             <div class="card-footer text-center">
+                 <a href="buy.php" class="btn btn-success btn-sm">Add to Cart</a>
+             </div><!--end card footer-->
+            </div><!--end card -->
+        </div><!--end col sizes -->
+     
+     
+        <!-- Class 2 -->
+        <!--  -->
+        <div class="col-lg-4 col-md-6 mb-4">
+         <div class="card h-100">
+             <a href="#"><img src="img/imageService.jpg" alt="" title="" class="card-img-top"></a>
+             <div class="card-body">
+             <h5 class="card-title"><a href="#">Iphone X</a></h5>
+                 <h6 class="text-muted">Description</h6>
+                 <p class="card-text">500Gb RAM , <br> 30pixel camera</p>
+                 <p>&#9733;&#9733;&#9733;&#9733;&#9733;&#9734;</p>
+             </div><!--end card body-->
+             <div class="card-footer text-center">
+                 <a href="buy.php" class="btn btn-success btn-sm">Add to Cart</a>
+             </div><!--end card footer-->
+         </div><!--end card -->
+        </div><!--end col sizes -->
+
+
+        <!-- Class 3 -->
+        <!--  -->
+        <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+            <a href="#"><img src="img/service_9.jpg" alt="" title="" class="card-img-top"></a>
+             <div class="card-body">
+             <h5 class="card-title"><a href="#">Iphone X</a></h5>
+                 <h6 class="text-muted">Description</h6>
+                 <p class="card-text">500Gb RAM , <br> 30pixel camera</p>
+                 <p>&#9733;&#9733;&#9733;&#9733;&#9733;&#9734;</p>
+             </div><!--end card body-->
+             <div class="card-footer text-center">
+                 <a href="buy.php" class="btn btn-success btn-sm">Add to Cart</a>
+             </div><!--end card footer-->
+         </div><!--end card -->
+        </div><!--end col sizes -->
+            
+            
+            
+        <!-- Class 4 -->
+        <!--  -->
+        <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+            <a href="#"><img src="img/imageService.jpg" alt="" title="" class="card-img-top"></a>
+             <div class="card-body">
+             <h5 class="card-title"><a href="#">Iphone X</a></h5>
+                 <h6 class="text-muted">Description</h6>
+                 <p class="card-text">500Gb RAM , <br> 30pixel camera</p>
+                 <p>&#9733;&#9733;&#9733;&#9733;&#9733;&#9734;</p>
+             </div><!--end card body-->
+             <div class="card-footer text-center">
+                 <a href="buy.php" class="btn btn-success btn-sm">Add to Cart</a>
+             </div><!--end card footer-->
+         </div><!--end card -->
+        </div><!--end col sizes -->
+            
+            
+        <!-- Class 5 -->
+        <!-- -->
+        <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+            <a href="#"><img src="img/service_9.jpg" alt="" title="" class="card-img-top"></a>
+             <div class="card-body">
+             <h5 class="card-title"><a href="#">Iphone X</a></h5>
+                 <h6 class="text-muted">Description</h6>
+                 <p class="card-text">500Gb RAM , <br> 30pixel camera</p>
+                 <p>&#9733;&#9733;&#9733;&#9733;&#9733;&#9734;</p>
+             </div><!--end card body-->
+             <div class="card-footer text-center">
+                 <a href="buy.php" class="btn btn-success btn-sm">Add to Cart</a>
+             </div><!--end card footer-->
+         </div><!--end card -->
+        </div><!--end col sizes -->
+            
+        <!-- Class 6 -->
+        <!--  -->
+        <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+            <a href="#"><img src="img/imageService.jpg" alt="" title="" class="card-img-top"></a>
+             <div class="card-body">
+             <h5 class="card-title"><a href="#">Iphone X</a></h5>
+                 <h6 class="text-muted">Description</h6>
+                 <p class="card-text">500Gb RAM , <br> 30pixel camera</p>
+                 <p>&#9733;&#9733;&#9733;&#9733;&#9733;&#9734;</p>
+             </div><!--end card body-->
+             <div class="card-footer text-center">
+                 <a href="buy.php" class="btn btn-success btn-sm">Add to Cart</a>
+             </div><!--end card footer-->
+         </div><!--end card -->
+        </div><!--end col sizes -->
+            
+        <!-- Class 7 -->
+        <!--  -->
+        <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+            <a href="#"><img src="img/service_9.jpg" alt="" title="" class="card-img-top"></a>
+             <div class="card-body">
+             <h5 class="card-title"><a href="#">Iphone X</a></h5>
+                 <h6 class="text-muted">Description</h6>
+                 <p class="card-text">500Gb RAM , <br> 30pixel camera</p>
+                 <p>&#9733;&#9733;&#9733;&#9733;&#9733;&#9734;</p>
+             </div><!--end card body-->
+             <div class="card-footer text-center">
+                 <a href="buy.php" class="btn btn-success btn-sm">Add to Cart</a>
+             </div><!--end card footer-->
+         </div><!--end card -->
+        </div><!--end col sizes -->
+
+            
+        <!-- Class 8 -->
+        <!--  -->
+        <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+            <a href="#"><img src="img/imageService.jpg" alt="" title="" class="card-img-top"></a>
+             <div class="card-body">
+             <h5 class="card-title"><a href="#">Iphone X</a></h5>
+                 <h6 class="text-muted">Description</h6>
+                 <p class="card-text">500Gb RAM , <br> 30pixel camera</p>
+                 <p>&#9733;&#9733;&#9733;&#9733;&#9733;&#9734;</p>
+             </div><!--end card body-->
+             <div class="card-footer text-center">
+                 <a href="buy.php" class="btn btn-success btn-sm">Add to Cart</a>
+             </div><!--end card footer-->
+         </div><!--end card -->
+        </div><!--end col sizes -->
+
+          
+        <!-- Class 9 -->
+        <!-- Version Control with Git and Github -->
+        <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+            <a href="#"><img src="img/service_9.jpg" alt="" title="" class="card-img-top"></a>
+             <div class="card-body">
+             <h5 class="card-title"><a href="#">Iphone X</a></h5>
+                 <h6 class="text-muted">Description</h6>
+                 <p class="card-text">500Gb RAM , <br> 30pixel camera</p>
+                 <p>&#9733;&#9733;&#9733;&#9733;&#9733;&#9734;</p>
+             </div><!--end card body-->
+             <div class="card-footer text-center">
+                 <a href="buy.php" class="btn btn-success btn-sm">Add to Cart</a>
+             </div><!--end card footer-->
+         </div><!--end card -->
+        </div><!--end col sizes -->
+     </div><!--end classes row -->
+
+     </div><!-- end right column -->
+ </div><!-- end 2 rows container --> 
+</div><!-- end 2 columns container --> 
+ <!-- end Two Columns -->
+  
+   <footer class="py-5 bg-dark text-muted text-center">
+       <div class="container">
+           <div class="btn-group btn-group-sm my-3" role="group" aria-label="Social Media Buttons">
+               <button class="btn btn-secondary" type="button"><i class="fa fa-snapchat-ghost"></i></button>
+               <button class="btn btn-secondary" type="button"><i class="fa fa-github-alt"></i></button>
+               <button class="btn btn-secondary" type="button"><i class="fa fa-google-plus"></i></button>
+               <button class="btn btn-secondary" type="button"><i class="fa fa-twitter"></i></button>
+           </div>
+           <small>
+           <p>&copy; <script>document.write(new Date().getFullYear());</script> Whoever. It sure is fun to create web sites.</p>
+            <p>&ldquo;Tell me and I forget, teach me and I may remember, involve me and I learn.&rdquo; &mdash; Benjamin Franklin </p>
+            </small>
+       </div>
+   </footer>
+   
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
